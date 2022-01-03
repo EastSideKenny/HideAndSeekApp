41d937cc476517148422530e159394292a7218a4
Jérôme JACOB <jerome56jj@hotmail.com>
2022-01-03 11:08:08 +0100

    Implemented GameController Class and Unit Tests

--- HideAndSeekApp/Program.cs
+++ HideAndSeekApp/Program.cs
@@ -6,7 +6,13 @@ namespace HideAndSeekApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameController gameController = new GameController();
+            while (true)
+            {
+                Console.WriteLine(gameController.Status);
+                Console.Write(gameController.Prompt);
+                Console.WriteLine(gameController.ParseInput(Console.ReadLine()));
+            }
         }
     }
 }
